f3734c5517d67705893351a957f2b0892e2c4c2c
Roger Infa <rinfas@ulasalle.edu.pe>
2025-04-24 19:46:57 -0500

    Arreglo sincronización

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,7 +20,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false, // No usar en producción
       }),
       inject: [ConfigService],
     }), UsuariosModule,
